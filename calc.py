dfa1274ebb378133b5b41ca0a309e27680beddce
ik <ghkdtnsdlr@naver.com>
2023-04-15 01:06:28 +0900

    modified calc

--- calc.py
+++ calc.py
@@ -3,20 +3,41 @@
 def plus(x, y):
     return x+y
 
+def minus(x, y):
+    return x-y
+
+def multiplycation(x, y):
+    return x*y
+
+def division(x, y):
+    return x/y
+
+# input two number
+def PutNumber():
+    global x, y
+    print("First Number")
+    x = int(input())
+    print("Second Number")
+    y = int(input())
+
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
-        print("0: exit, 1: plus")
+    while check >= 1:
+        print("0: exit, 1: plus, 2: minus, 3: multiplycation, 4: division")
         check = int(input())
-        if check == 1:
-            print("First Number")
-            x = int(input())
-            print("Second Number")
-            y = int(input())
-            print("answer : ", plus(x,y))
-        elif check > 1:
+        if check >= 1 or check <= 4:
+            PutNumber()
+	    if check == 1:
+	       	print("answer : ", plus(x,y))
+            elif check == 2:
+		print("answer : ", minus(x,y))
+	    elif check == 3:
+	       	print("answer : ", multiplycation(x,y))
+	    elif check == 4:
+	       	print("answer : ", division(x,y))
+        elif check > 4:
             print("Unsupported")
         else:
             print("Thank you")
